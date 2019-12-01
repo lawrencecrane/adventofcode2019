ebbd1fba2c86a59342fd869e0b9b84e8b14ae004
Lauri Kurki <lauri.kurki@siili.com>
2019-12-01 12:56:12 +0200

    Solve day 1 first star

--- 01/main.go
+++ 01/main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"math"
+	"os"
+	"strconv"
 )
 
 func main() {
+	f, err := os.Open("input")
+
+	check(err)
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	sum := 0
+
+	for scanner.Scan() {
+		x, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+		sum += FuelCounterUpper(int(x))
+	}
+
+	fmt.Printf("Answer for Part 1: %d\n", sum)
 }
 
 func FuelCounterUpper(mass int) int {
 	return int(math.Floor(float64(mass)/3)) - 2
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
