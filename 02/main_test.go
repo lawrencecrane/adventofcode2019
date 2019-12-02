cca7b4acf842f8876373597a9fd5130879361d1f
Lauri Kurki <lauri.kurki@siili.com>
2019-12-02 10:11:31 +0200

    Add tests for Day 02 Part 1

---
+++ 02/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestExec(t *testing.T) {
+	Helper(t, exec, []int{1, 0, 0, 0, 99}, []int{2, 0, 0, 0, 99})
+	Helper(t, exec, []int{2, 3, 0, 3, 99}, []int{2, 3, 0, 6, 99})
+	Helper(t, exec, []int{2, 4, 4, 5, 99, 0}, []int{2, 4, 4, 5, 99, 9801})
+	Helper(t, exec, []int{1, 1, 1, 4, 99, 5, 6, 0, 99}, []int{30, 1, 1, 4, 2, 5, 6, 0, 99})
+}
+
+func Helper(t *testing.T, f func([]int) []int, x, expected []int) {
+	ans := f(x)
+
+	if !Equal(ans, expected) {
+		t.Errorf("Expected %v, Got %v", expected, ans)
+	}
+}
+
+func Equal(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
