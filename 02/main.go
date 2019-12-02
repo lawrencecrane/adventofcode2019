542e462582ee6704049028fe6547662d1d118677
Lauri Kurki <lauri.kurki@siili.com>
2019-12-02 10:31:48 +0200

    Solve part one for day two

--- 02/main.go
+++ 02/main.go
@@ -9,17 +9,47 @@ import (
 )
 
 func main() {
-	for _, value := range input(solve) {
-		fmt.Println(value)
-	}
+	fmt.Printf("Answer to Part 1: %d\n", input(solve)[0])
 }
 
 func solve(in string) []int {
-	return exec(parse(split(in)))
+	stack := parse(split(in))
+
+	stack[1] = 12
+	stack[2] = 2
+
+	return exec(stack)
 }
 
-func exec(in []int) []int {
-	return in
+func exec(stack []int) []int {
+	stack, _ = execHelper(stack, 0)
+	return stack
+}
+
+func execHelper(stack []int, pos int) ([]int, int) {
+	switch stack[pos] {
+	case 1:
+		return execHelper(execOpcode(pos, add, stack), pos+4)
+	case 2:
+		return execHelper(execOpcode(pos, multiply, stack), pos+4)
+	case 99:
+		return stack, pos
+	default:
+		panic("Unknown opcode")
+	}
+}
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+func add(a, b int) int {
+	return a + b
+}
+
+func execOpcode(pos int, fun func(int, int) int, stack []int) []int {
+	stack[stack[pos+3]] = fun(stack[stack[pos+1]], stack[stack[pos+2]])
+	return stack
 }
 
 func parse(in []string) []int {
