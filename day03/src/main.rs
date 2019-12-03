bac60a6246144f9cf8cedaf54a746d1209ef11b9
Lauri Kurki <lauri.kurki@siili.com>
2019-12-03 19:43:08 +0200

    Read input for day03

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,2 +1,18 @@
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::fs::File;
+
+
 fn main() {
+    input()
 }
+
+fn input() {
+    let f = File::open("data/input").unwrap();
+    let f = BufReader::new(f);
+
+    for line in f.lines() {
+        println!("{}", line.unwrap())
+    }
+}
+
