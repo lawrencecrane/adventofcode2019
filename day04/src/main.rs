9ded110c78e3b80fcae54d0c7dac3e600f6815a0
Lauri Kurki <lauri.kurki@siili.com>
2019-12-04 18:31:48 +0200

    Parse input for day 04

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,2 +1,12 @@
 fn main() {
+    println!("{:?}", parse("137683-596253").unwrap());
+}
+
+fn parse(input: &str) -> Option<(usize, usize)> {
+    let mut splitted = input.split('-');
+
+    match (splitted.next(), splitted.next()) {
+        (Some(x), Some(y)) => Some((x.parse().unwrap(), y.parse().unwrap())),
+        _ => None
+    }
 }
