bf8d52cea466d0d98605071108d90518db4a21be
Lauri Kurki <lauri.kurki@siili.com>
2019-12-06 20:48:00 +0200

    Part 2 Day 6 solved

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -5,7 +5,56 @@ use std::collections::HashMap;
 
 fn main() {
     let map = input(&parse);
+
     println!("Total number of direct and indirect orbits {}", orbit_count(&map));
+    println!("Minimum number of orbital transfers {}",
+             find_shortest(&map, &String::from("YOU"), &String::from("SAN")));
+}
+
+fn find_shortest(map: &HashMap<String, Vec<String>>, a: &String, b: &String) -> usize {
+    let mut steps = HashMap::new();
+
+    let mut a_parents = map.get(a).unwrap().to_owned();
+    let mut b_parents = map.get(b).unwrap().to_owned();
+
+    for step in 0.. {
+        match add_step_count(&mut steps, &a_parents, &b_parents, step) {
+            Some(count) => return count,
+            _ => ()
+        }
+
+        a_parents = combine_parents(a_parents, map);
+        b_parents = combine_parents(b_parents, map);
+    }
+
+    return 0
+}
+
+fn add_step_count(steps: &mut HashMap<String, usize>,
+                  nodes_a: &Vec<String>,
+                  nodes_b: &Vec<String>,
+                  step: usize) -> Option<usize> {
+    let (_, final_count) = nodes_a.iter()
+        .chain(nodes_b.iter())
+        .fold((steps, None), |(steps, n), node| {
+            if steps.contains_key(node) {
+                let count = steps.get(node).unwrap() + step;
+                return (steps, Some(count))
+            }
+
+            steps.insert(node.to_owned(), step);
+
+            (steps, n)
+        });
+
+    return final_count
+}
+
+fn combine_parents(childs: Vec<String>, map: &HashMap<String, Vec<String>>) -> Vec<String> {
+    childs.iter().fold(Vec::new(), |mut parents, child| {
+        parents.extend_from_slice(map.get(child).unwrap());
+        parents
+    })
 }
 
 fn orbit_count(map: &HashMap<String, Vec<String>>) -> usize {
@@ -80,6 +129,12 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_find_shortest() {
+        let map = test_data2();
+        assert_eq!(find_shortest(&map, &String::from("YOU"), &String::from("SAN")), 4)
+    }
+
+    #[test]
     fn test_indirect_orbit_count() {
         let map = test_data();
         let counts = direct_orbit_counts(&map);
@@ -91,6 +146,19 @@ mod tests {
         let map = test_data();
         let counts = direct_orbit_counts(&map);
         assert_eq!(direct_orbit_count(&counts), 11)
+    }
+
+    fn test_data2() -> HashMap<String, Vec<String>> {
+        let data = ["COM)B", "B)C", "C)D", "D)E", "E)F",
+            "B)G", "G)H", "D)I", "E)J", "J)K","K)L", "K)YOU", "I)SAN"];
+
+        let mut map = HashMap::new();
+
+        for val in data.iter() {
+            parse(String::from(*val), &mut map);
+        }
+
+        map
     }
 
     fn test_data() -> HashMap<String, Vec<String>> {
@@ -105,5 +173,4 @@ mod tests {
 
         map
     }
-
 }
