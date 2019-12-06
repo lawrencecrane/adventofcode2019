4e0ee100604c1213c1c25d82050be2655ab6abe8
Lauri Kurki <lauri.kurki@siili.com>
2019-12-06 18:23:12 +0200

    Solve part 1 day 6

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -5,13 +5,40 @@ use std::collections::HashMap;
 
 fn main() {
     let map = input(&parse);
-    println!("{:?}", map);
+    println!("Total number of direct and indirect orbits {}", orbit_count(&map));
 }
 
-fn direct_orbit_count(map: &HashMap<String, Vec<String>>) -> usize {
+fn orbit_count(map: &HashMap<String, Vec<String>>) -> usize {
+    let counts = direct_orbit_counts(&map);
+    direct_orbit_count(&counts) + indirect_orbit_total_count(map, &counts)
+}
+
+fn indirect_orbit_total_count(map: &HashMap<String, Vec<String>>,
+                              counts: &HashMap<&String, usize>) -> usize {
+    map.keys().map(|val| indirect_orbit_count(val, map, counts)).sum()
+}
+
+fn indirect_orbit_count(val: &String,
+                        map: &HashMap<String, Vec<String>>,
+                        counts: &HashMap<&String, usize>) -> usize {
+   let parents = map.get(val).unwrap();
+
+   let sum: usize = parents.iter().map(|x| counts.get(x).unwrap()).sum();
+   let sum2: usize = parents.iter().map(|x| indirect_orbit_count(x, map, counts)).sum();
+
+   sum + sum2
+}
+
+fn direct_orbit_count(counts: &HashMap<&String, usize>) -> usize {
+    counts.values().sum()
+}
+
+fn direct_orbit_counts(map: &HashMap<String, Vec<String>>) -> HashMap<&String, usize> {
     map.iter()
-        .map(|(_, val)| val.len())
-        .sum()
+        .fold(HashMap::new(), |mut map, (key, parents)| {
+            map.insert(key, parents.len());
+            map
+        })
 }
 
 fn add_to_orbit_map(map: &mut HashMap<String, Vec<String>>,
@@ -53,7 +80,20 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_indirect_orbit_count() {
+        let map = test_data();
+        let counts = direct_orbit_counts(&map);
+        assert_eq!(indirect_orbit_total_count(&map, &counts), 31)
+    }
+
+    #[test]
     fn test_direct_orbit_count() {
+        let map = test_data();
+        let counts = direct_orbit_counts(&map);
+        assert_eq!(direct_orbit_count(&counts), 11)
+    }
+
+    fn test_data() -> HashMap<String, Vec<String>> {
         let data = ["COM)B", "B)C", "C)D", "D)E", "E)F",
             "B)G", "G)H", "D)I", "E)J", "J)K","K)L"];
 
@@ -63,6 +103,7 @@ mod tests {
             parse(String::from(*val), &mut map);
         }
 
-        assert_eq!(direct_orbit_count(&map), 11)
+        map
     }
+
 }
