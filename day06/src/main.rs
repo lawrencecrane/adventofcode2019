1843ef3e45de0b2b3dde106982fbc78b66f9d2f7
Lauri Kurki <lauri.kurki@siili.com>
2019-12-06 13:33:17 +0200

    Read input

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,2 +1,21 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
 fn main() {
+    println!("{:?}", input())
+}
+
+fn input() -> Vec<String> {
+    let f = File::open("data/input").unwrap();
+    let f = BufReader::new(f);
+
+    let mut out = Vec::new();
+
+    for line in f.lines() {
+        let data = line.unwrap();
+        out.push(data);
+    }
+
+    out
 }
