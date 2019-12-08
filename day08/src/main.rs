e629505e403b48ce8114f7adc363a777f24c5eea
Lauri Kurki <lauri.kurki@siili.com>
2019-12-08 13:07:39 +0200

    Decode & print image
    Part 2 for Day 08 solved

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -5,9 +5,48 @@ use std::io::prelude::*;
 type Matrix = Vec<Vec<usize>>;
 
 fn main() {
-    let layers = to_layers(input(), 25, 6);
-    let layer = find_layer_with_fewest_zeros(&layers);
-    println!("Answer to Part 1: {}", multiply_counts_of(1, 2, layer))
+    let size = 25 * 6;
+    let layers = to_layers(input(), size);
+
+    println!("Answer to Part 1: {}",
+             multiply_counts_of(1, 2, find_layer_with_fewest_zeros(&layers)));
+
+    let image = to_image(&layers, size);
+
+    println!("Decoded image:");
+    print_image(&image, 25, 6);
+}
+
+fn print_image(image: &Vec<usize>, width: usize, height: usize) {
+    for h in 0..height {
+        let row = image[h*width..(h*width + width)].iter()
+                 .map(|x| {
+                     match x {
+                         0 => ' ',
+                         _ => 'X'
+                     }
+                 });
+
+        for pixel in row {
+            print!("{}", pixel);
+        }
+
+        println!();
+    }
+}
+
+fn to_image(layers: &Matrix, size: usize) -> Vec<usize> {
+    (0..size)
+        .fold(Vec::new(), |mut img, pixel| {
+            let pxl = layers.iter()
+                .map(|layer| layer[pixel])
+                .find(|pxl| pxl != &2)
+                .unwrap();
+
+            img.push(pxl);
+
+            img
+        })
 }
 
 fn multiply_counts_of(a: usize, b: usize, layer: &Vec<usize>) -> usize {
@@ -32,9 +71,7 @@ fn count_of(a: usize, layer: &Vec<usize>) -> usize {
     layer.iter().filter(|x| x == &&a).count()
 }
 
-fn to_layers(mut data: String, width: usize, height: usize) -> Matrix {
-    let size = width * height;
-
+fn to_layers(mut data: String, size: usize) -> Matrix {
     (0..data.len() / size)
         .map(|_| data.drain(0..size)
              .map(|x| x.to_digit(10).unwrap() as usize).collect())
