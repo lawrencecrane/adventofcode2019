920b79ad7077ffe66eb22f16725292b0d79b445c
Lauri Kurki <lauri.kurki@siili.com>
2019-12-08 12:12:28 +0200

    Parse data to matrix & layers

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -2,8 +2,26 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 
+type Layer = Vec<Matrix>;
+type Matrix = Vec<Vec<usize>>;
+
 fn main() {
-    println!("{}", input())
+    println!("{:?}", to_layers(input(), 25, 6))
+}
+
+fn to_layers(mut data: String, width: usize, height: usize) -> Layer {
+    let size = width * height;
+
+    (0..data.len() / size)
+        .map(|_| to_matrix(data.drain(0..size).collect(), width, height))
+        .collect()
+}
+
+fn to_matrix(mut data: String, width: usize, height: usize) -> Matrix {
+    (0..height)
+        .map(|_| data.drain(0..width)
+             .map(|x| x.to_digit(10).unwrap() as usize).collect())
+        .collect()
 }
 
 fn input() -> String {
