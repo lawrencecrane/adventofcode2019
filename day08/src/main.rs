a308b37cb202a68f08775bc211b2704c9aef6865
Lauri Kurki <lauri.kurki@siili.com>
2019-12-08 11:46:08 +0200

    Read input

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,2 +1,23 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
 fn main() {
+    println!("{}", input())
 }
+
+fn input() -> String {
+    let f = File::open("data/input").unwrap();
+    let f = BufReader::new(f);
+
+    f.lines().next().unwrap().unwrap()
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_fn() {
+//     }
+// }
