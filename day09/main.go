dccc066feb9aa40d18dbe0a0b49fc778195e6971
Lauri Kurki <lauri.kurki@siili.com>
2019-12-09 18:46:26 +0200

    Copy code from day07 and simplify it tiny bit

---
+++ day09/main.go
@@ -0,0 +1,369 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Opcodes
+const (
+	ADD      = 1
+	MULTIPLY = 2
+	INPUT    = 3
+	OUTPUT   = 4
+	JUMPT    = 5
+	JUMPF    = 6
+	LESS     = 7
+	EQUALS   = 8
+	HALT     = 99
+)
+
+// Modes
+const (
+	POSITION  = 0
+	IMMEDIATE = 1
+)
+
+type amplifier struct {
+	addr   int
+	halted bool
+	input  []int
+	output int
+	stack  []int
+}
+
+func main() {
+	stack := parse(split(loadInput()))
+
+	signal, phase := findMostAmplified(stack,
+		execCircuit,
+		[]int{0, 1, 2, 3, 4})
+
+	fmt.Printf("Most Amplifed signal is %v\n", signal)
+	fmt.Printf("Most Amplifed phase is %v\n", phase)
+
+	signal2, phase2 := findMostAmplified(stack,
+		execFeedbackLoop,
+		[]int{5, 6, 7, 8, 9})
+
+	fmt.Printf("Most Amplifed feedbacked signal is %v\n", signal2)
+	fmt.Printf("Most Amplifed feedbacked phase is %v\n", phase2)
+}
+
+func findMostAmplified(stack []int,
+	fun func([]amplifier, int) ([]amplifier, bool),
+	phaseValues []int) (int, []int) {
+	max := 0
+	imax := 0
+
+	permutations := permutationsHeap(phaseValues)
+
+	for i, phases := range permutations {
+		amps := createAmplifiers(stack, phases)
+		executed, _ := fun(amps, 0)
+
+		if executed[len(executed)-1].output > max {
+			max = executed[len(executed)-1].output
+			imax = i
+		}
+	}
+
+	return max, permutations[imax]
+}
+
+func createAmplifiers(stack []int, phases []int) []amplifier {
+	amps := make([]amplifier, len(phases))
+
+	for i, phase := range phases {
+		tmp := make([]int, len(stack))
+		copy(tmp, stack)
+
+		amps[i] = amplifier{
+			addr:   0,
+			halted: false,
+			input:  []int{phase},
+			output: 0,
+			stack:  tmp,
+		}
+	}
+
+	return amps
+}
+
+func execFeedbackLoop(amps []amplifier, input int) ([]amplifier, bool) {
+	for {
+		executed, halted := execCircuit(amps, input)
+
+		if halted {
+			return executed, true
+		}
+
+		amps = executed
+		input = executed[len(executed)-1].output
+	}
+
+	return amps, false
+}
+
+func execCircuit(amps []amplifier, input int) ([]amplifier, bool) {
+	for i, amp := range amps {
+		amp.input = append(amp.input, input)
+		executed, err := exec(amp)
+
+		amps[i] = executed
+
+		if err != nil {
+			return nil, false
+		}
+
+		if executed.halted {
+			return amps, true
+		}
+
+		input = executed.output
+	}
+
+	return amps, false
+}
+
+func exec(amp amplifier) (amplifier, error) {
+	opcode, modes := parseInstruction(amp.stack[amp.addr])
+
+	switch opcode {
+	case ADD:
+		amp = calc(amp, modes, add)
+	case MULTIPLY:
+		amp = calc(amp, modes, mult)
+	case LESS:
+		amp = calc(amp, modes, ifLess)
+	case EQUALS:
+		amp = calc(amp, modes, ifEqual)
+	case JUMPT:
+		amp = jump(amp, modes, true)
+	case JUMPF:
+		amp = jump(amp, modes, false)
+	case INPUT:
+		amp = read(amp)
+	case OUTPUT:
+		amp = out(amp, modes)
+		return amp, nil
+	case HALT:
+		amp.halted = true
+		return amp, nil
+	default:
+		return amp, errors.New("Unknown opcode")
+	}
+
+	return exec(amp)
+}
+
+func jump(amp amplifier, modes []int, cmp bool) amplifier {
+	fFst, fSnd := pairModeToFuncs(modes[0], modes[1])
+
+	if (fFst(amp.stack, amp.addr+1) != 0) == cmp {
+		amp.addr = fSnd(amp.stack, amp.addr+2)
+	} else {
+		amp.addr += 3
+	}
+
+	return amp
+}
+
+func calc(amp amplifier, modes []int, f func(int, int) int) amplifier {
+	fFst, fSnd := pairModeToFuncs(modes[0], modes[1])
+
+	res := f(fFst(amp.stack, amp.addr+1), fSnd(amp.stack, amp.addr+2))
+	positionModeWrite(amp.stack, amp.addr+3, res)
+
+	amp.addr += 4
+
+	return amp
+}
+
+func out(amp amplifier, modes []int) amplifier {
+	fun, _ := modeToFunc(modes[0])
+
+	amp.output = fun(amp.stack, amp.addr+1)
+	amp.addr += 2
+
+	return amp
+}
+
+func read(amp amplifier) amplifier {
+	positionModeWrite(amp.stack, amp.addr+1, amp.input[0])
+
+	amp.input = amp.input[1:]
+	amp.addr += 2
+
+	return amp
+}
+
+func ifLess(a, b int) int {
+	if a < b {
+		return 1
+	}
+
+	return 0
+}
+
+func ifEqual(a, b int) int {
+	if a == b {
+		return 1
+	}
+
+	return 0
+}
+
+func add(a, b int) int {
+	return a + b
+}
+
+func mult(a, b int) int {
+	return a * b
+}
+
+func pairModeToFuncs(a, b int) (func([]int, int) int, func([]int, int) int) {
+	af, _ := modeToFunc(a)
+	bf, _ := modeToFunc(b)
+
+	return af, bf
+}
+
+func modeToFunc(mode int) (func([]int, int) int, error) {
+	switch mode {
+	case POSITION:
+		return positionModeRead, nil
+	case IMMEDIATE:
+		return immediateModeRead, nil
+	default:
+		return nil, errors.New("Unknown mode")
+	}
+}
+
+func parseInstruction(x int) (int, []int) {
+	modes := parseModes(x)
+	modes = addTrailingZeros(modes, 3-len(modes))
+
+	opcode := modes[0] + 10*modes[1]
+
+	return opcode, addPaddingToModes(opcode, modes[2:])
+}
+
+func addPaddingToModes(opcode int, modes []int) []int {
+	switch opcode {
+	case
+		ADD,
+		MULTIPLY,
+		LESS,
+		EQUALS:
+		return addTrailingZeros(modes, 3-len(modes))
+	case
+		JUMPT,
+		JUMPF:
+		return addTrailingZeros(modes, 2-len(modes))
+	case OUTPUT:
+		return addTrailingZeros(modes, 1-len(modes))
+	default:
+		return modes
+	}
+}
+
+func addTrailingZeros(modes []int, n int) []int {
+	for i := 0; i < n; i++ {
+		modes = append(modes, 0)
+	}
+
+	return modes
+}
+
+func parseModes(x int) []int {
+	var modes []int
+
+	for x >= 1 {
+		modes = append(modes, x%10)
+		x = x / 10
+	}
+
+	return modes
+}
+
+func positionModeWrite(stack []int, pos, value int) {
+	stack[immediateModeRead(stack, pos)] = value
+}
+
+func positionModeRead(stack []int, pos int) int {
+	return stack[immediateModeRead(stack, pos)]
+}
+
+func immediateModeRead(stack []int, pos int) int {
+	return stack[pos]
+}
+
+func permutationsHeap(xs []int) [][]int {
+	return permutationsHeapHelper(len(xs), xs, [][]int{})
+}
+
+func permutationsHeapHelper(n int, xs []int, permutations [][]int) [][]int {
+	if n == 1 {
+		tmp := make([]int, len(xs))
+		copy(tmp, xs)
+		return append(permutations, tmp)
+	}
+
+	var res [][]int
+
+	for i := 0; i < n; i++ {
+		res = append(res, permutationsHeapHelper(n-1, xs, permutations)...)
+
+		if n%2 == 0 {
+			swap(xs, i, n-1)
+		} else {
+			swap(xs, 0, n-1)
+		}
+	}
+
+	return res
+}
+
+func swap(xs []int, i, j int) {
+	tmp := xs[i]
+	xs[i] = xs[j]
+	xs[j] = tmp
+}
+
+func parse(in []string) []int {
+	out := make([]int, len(in))
+
+	for key, value := range in {
+		parsed, _ := strconv.ParseInt(value, 10, 32)
+		out[key] = int(parsed)
+	}
+
+	return out
+}
+
+func split(in string) []string {
+	return strings.Split(in, ",")
+}
+
+func loadInput() string {
+	f, err := os.Open("input")
+
+	check(err)
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Scan()
+
+	return scanner.Text()
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
---
+++ day09/main_test.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestFindMostAmplfied(t *testing.T) {
+	FindMostAmplifiedTestHelper(t,
+		execCircuit,
+		[]int{0, 1, 2, 3, 4},
+		[]int{3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0},
+		[]int{4, 3, 2, 1, 0},
+		43210)
+
+	FindMostAmplifiedTestHelper(t,
+		execCircuit,
+		[]int{0, 1, 2, 3, 4},
+		[]int{3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0},
+		[]int{0, 1, 2, 3, 4},
+		54321)
+
+	FindMostAmplifiedTestHelper(t,
+		execCircuit,
+		[]int{0, 1, 2, 3, 4},
+		[]int{3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0},
+		[]int{1, 0, 4, 3, 2},
+		65210)
+
+	FindMostAmplifiedTestHelper(t,
+		execFeedbackLoop,
+		[]int{5, 6, 7, 8, 9},
+		[]int{3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5},
+		[]int{9, 8, 7, 6, 5},
+		139629729)
+
+	FindMostAmplifiedTestHelper(t,
+		execFeedbackLoop,
+		[]int{5, 6, 7, 8, 9},
+		[]int{3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54,
+			-5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4,
+			53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10},
+		[]int{9, 7, 8, 5, 6}, 18216)
+}
+
+func TestExecFeedbackLoop(t *testing.T) {
+	ExecFeedbackLoopTestHelper(t,
+		[]int{3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5},
+		[]int{9, 8, 7, 6, 5},
+		139629729)
+
+	ExecFeedbackLoopTestHelper(t,
+		[]int{3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54,
+			-5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4,
+			53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10},
+		[]int{9, 7, 8, 5, 6}, 18216)
+}
+
+func TestExecCircuit(t *testing.T) {
+	ExecCircuitTestHelper(t,
+		[]int{3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0},
+		[]int{4, 3, 2, 1, 0},
+		43210)
+
+	ExecCircuitTestHelper(t,
+		[]int{3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0},
+		[]int{0, 1, 2, 3, 4},
+		54321)
+
+	ExecCircuitTestHelper(t,
+		[]int{3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0},
+		[]int{1, 0, 4, 3, 2},
+		65210)
+}
+
+func TestParseInstruction(t *testing.T) {
+	ParseInstructionTestHelper(t, 1234, 34, []int{2, 1})
+	ParseInstructionTestHelper(t, 1203004, 4, []int{0, 3, 0, 2, 1})
+
+}
+
+func ExecFeedbackLoopTestHelper(t *testing.T, stack, phases []int, expected int) {
+	amps := createAmplifiers(stack, phases)
+	executed, _ := execFeedbackLoop(amps, 0)
+
+	signal := executed[len(executed)-1].output
+
+	if signal != expected {
+		t.Errorf("Expected: %v, Got %v", expected, signal)
+	}
+}
+
+func FindMostAmplifiedTestHelper(t *testing.T,
+	fun func([]amplifier, int) ([]amplifier, bool),
+	phaseValues []int,
+	stack,
+	expectedPhase []int,
+	expectedSignal int) {
+	signal, phase := findMostAmplified(stack, fun, phaseValues)
+
+	if signal != expectedSignal || !Equal(phase, expectedPhase) {
+		t.Errorf("Expected: %v, Got %v -- Expected: %v, Got: %v",
+			expectedSignal,
+			signal,
+			expectedPhase,
+			phase)
+	}
+}
+
+func ExecCircuitTestHelper(t *testing.T, stack, phases []int, expected int) {
+	amps := createAmplifiers(stack, phases)
+	executed, _ := execCircuit(amps, 0)
+
+	signal := executed[len(executed)-1].output
+
+	if signal != expected {
+		t.Errorf("Expected: %v, Got %v", expected, signal)
+	}
+}
+
+func ParseInstructionTestHelper(t *testing.T, x, expectedOpcode int, expectedModes []int) {
+	opcode, modes := parseInstruction(x)
+
+	if !Equal(modes, expectedModes) {
+		t.Errorf("Expected %v, Got %v", expectedModes, modes)
+	}
+
+	if opcode != expectedOpcode {
+		t.Errorf("Expected %v, Got %v", expectedOpcode, opcode)
+	}
+}
+
+func Equal(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
